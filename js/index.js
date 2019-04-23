6c367aeace3bd211c9d6b4b45d9d1f156564e98b
Gokul S <iamsgokul@gmail.com>
2019-04-23 19:39:33 +0530

    Chat Bar animation

---
+++ js/index.js
@@ -0,0 +1,6 @@
+       $(document).ready(function () {
+            $('.chatplus a').click(function () {
+                $('.chatctrl').toggleClass('active');
+                $('.chatbox').toggleClass("open close");  
+            })
+        })
